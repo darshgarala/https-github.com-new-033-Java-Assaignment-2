0295d62b0cbee8a9a8989b842e51bb5e16a7c8e1
darshgarala <110333279+darshgarala@users.noreply.github.com>
2022-08-05 19:15:44 +0530

    Create P_2main.java

---
+++ P_2main.java
@@ -0,0 +1,114 @@
+public class P_2 {
+    
+
+    public static void main(String[] args)
+    {
+    System.out.println("An example for you to to create a proper account :");
+        Account a1 =new Account();
+        a1.Accessor();
+        Scanner s = new Scanner(System.in);
+        
+        int id;
+        double balance,withdraw,deposit,monintrate,monint;
+        String date;
+        System.out.println("Enter the Account number of your account  :");
+        id = s.nextInt();
+        System.out.println("Enter the initial balance your account  :");
+        balance = s.nextDouble();
+        System.out.println("Enter the date at which you created your account  :");
+        date = s.next();
+        
+        Account a2 = new Account(id, balance, date);
+        a2.Accessor();
+        monintrate = a2.getMonthlyInterestRate();
+        monint = a2.getMonthlyInterest();
+        System.out.println("Bank give "+monintrate+"% monthly intrest rate.");
+        System.out.println("Your monthly intrest is "+monint+" Rupees");
+        int i;
+
+        System.out.println("Enter 1 to withdraw and 2 to deposit.");
+        i = s.nextInt();
+        switch(i)
+        {
+            case 1 :
+            {
+                System.out.println("Enter amount to be withdrawn  :");
+                withdraw = s.nextDouble();
+                a2.withdraw(withdraw);
+                System.out.println("The amount remained in your account after withdrawal is   :"+a2.balance);
+                break;
+            }
+            case 2 :
+            {
+                System.out.println("Enter amount to be deposited  :");
+                deposit = s.nextDouble();
+                balance = a2.deposit(deposit);
+                System.out.println("The amount remained in your account after deposit is   :"+balance);
+                break;
+            }
+            default :
+            {
+                System.out.println("You have changed anything  :");
+                break;
+            }
+        }
+        System.out.println("Your account afer withdrawal or deposit  :");
+        a2.Accessor();
+
+        int p=1;
+        while(p==1)
+        {
+            System.out.println("Enter 1 use another account and 2 to not.");
+            i = s.nextInt();
+            if(i==1)
+            {
+                System.out.println("Enter the Account number of your account  :");
+                id = s.nextInt();
+                System.out.println("Enter the initial balance your account  :");
+                balance = s.nextDouble();
+                System.out.println("Enter the date at which you created your account  :");
+                date = s.next();
+
+                a2.mutator(id, balance, date);
+                a2.Accessor();
+                monintrate = a2.getMonthlyInterestRate();
+                monint = a2.getMonthlyInterest();
+                System.out.println("Bank give "+monintrate+"% monthly intrest rate.");
+                System.out.println("Your monthly intrest is "+monint+" Rupees");
+                System.out.println("Enter 1 to withdraw and 2 to deposit.");
+                i = s.nextInt();
+                
+                switch(i)
+                {
+                    case 1 :
+                    {
+                        System.out.println("Enter amount to be withdrawn  :");
+                        withdraw = s.nextDouble();
+                        a2.withdraw(withdraw);
+                        System.out.println("The amount remained in your account after withdrawal is   :"+a2.balance);
+                        break;
+                    }
+                    case 2 :
+                    {
+                        System.out.println("Enter amount to be deposited  :");
+                        deposit = s.nextDouble();
+                        balance = a2.deposit(deposit);
+                        System.out.println("The amount remained in your account after deposit is   :"+balance);
+                        break;
+                    }
+                    default :
+                    {
+                        System.out.println("You have changed anything  :");
+                        break;
+                    }
+                }
+            }
+            else 
+            {
+                System.out.println("--------------thanks for coming---------------------");
+                break;
+            }
+        }
+ 
+    }
+}
